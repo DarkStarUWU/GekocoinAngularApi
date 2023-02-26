e3042d5bb40df643c9ba0e33f0d201d5dba7d124
DarkStarUWU <guevara4536@hotmail.com>
2023-02-26 13:32:49 -0500

    Creando conexion y mostrar en pantalla la informacion de json en pantalla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+
+interface coin{
+  id:string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percentage_24: number;
+  total_volume: number;
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'proyecto-angular-gekkocoin';
+
+
+export class AppComponent implements OnInit{
+
+  coins: coin[] = [];
+
+
+  constructor(private http: HttpClient) {};
+
+  ngOnInit() {
+    
+    this.http.get<coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').subscribe(
+      (res) =>{
+        console.log(res);
+        this.coins = res;
+      },
+      (arr)=> console.log(arr)
+
+    )
+  }
+
 }
