e9b810ef293a45478ddc5ead6337de65391eec62
DarkStarUWU <guevara4536@hotmail.com>
2023-02-26 19:28:38 -0500

    Mejora para la vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ interface coin{
   symbol: string;
   image: string;
   current_price: number;
-  price_change_percentage_24: number;
+  price_change_percentage_24h: number;
   total_volume: number;
 }
 
@@ -23,12 +23,19 @@ interface coin{
 export class AppComponent implements OnInit{
 
   coins: coin[] = [];
+  titles: string [ ] = [
+    '#',
+    'coin',
+    'Price',
+    'Price Change',
+    '24h Volume',
 
+  ]
 
   constructor(private http: HttpClient) {};
 
   ngOnInit() {
-    
+
     this.http.get<coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').subscribe(
       (res) =>{
         console.log(res);
